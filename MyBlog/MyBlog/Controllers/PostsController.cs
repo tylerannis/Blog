6d584b4c1685bd6a48b9d0890543da95759ad34d
SeedPaths-User <lessdismissive@email.com>
2014-10-08 13:45:55 -0600

    Blog

---
+++ MyBlog/MyBlog/Controllers/PostsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyBlog.Models;
+
+namespace MyBlog.Controllers
+{
+    public class PostsController : Controller
+    {
+        private BlogEntities db = new BlogEntities();
+
+        //
+        // GET: /Posts/
+
+        public ActionResult Index()
+        {
+            var posts = db.Posts.Include(p => p.Author);
+            return View(posts.ToList());
+        }
+
+        //
+        // GET: /Posts/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Post post = db.Posts.Find(id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            return View(post);
+        }
+
+        //
+        // GET: /Posts/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.AuthorID = new SelectList(db.Authors, "AuthorID", "Name");
+            return View();
+        }
+
+        //
+        // POST: /Posts/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Posts.Add(post);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.AuthorID = new SelectList(db.Authors, "AuthorID", "Name", post.AuthorID);
+            return View(post);
+        }
+
+        //
+        // GET: /Posts/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Post post = db.Posts.Find(id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.AuthorID = new SelectList(db.Authors, "AuthorID", "Name", post.AuthorID);
+            return View(post);
+        }
+
+        //
+        // POST: /Posts/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(post).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.AuthorID = new SelectList(db.Authors, "AuthorID", "Name", post.AuthorID);
+            return View(post);
+        }
+
+        //
+        // GET: /Posts/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Post post = db.Posts.Find(id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            return View(post);
+        }
+
+        //
+        // POST: /Posts/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Post post = db.Posts.Find(id);
+            db.Posts.Remove(post);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
